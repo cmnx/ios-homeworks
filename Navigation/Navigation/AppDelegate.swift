ecd16594896a86cf5ad53302600516d0d7e96e91
Constantin Novoslugin <cmn@email.com>
2022-05-06 00:55:13 +0500

    Change project life cycle. Use SceneDelegate instead of AppDelegate

--- Navigation/Navigation/AppDelegate.swift
+++ Navigation/Navigation/AppDelegate.swift
@@ -9,15 +9,21 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-    var window: UIWindow?
+    
+//    var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = TabBarViewController()
-        window?.makeKeyAndVisible()
+        
+//        window = UIWindow(frame: UIScreen.main.bounds)
+//        window?.rootViewController = TabBarViewController()
+//        window?.makeKeyAndVisible()
+
         return true
+    }
+    
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
   
